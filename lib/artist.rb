ef1be088a74eea04ac5ceabfa87b569e36680343
Mike Shahindoust <gobias101@gmail.com>
2020-08-21 21:21:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,18 @@
 class Artist
   
-  attr_accessor :name, :songs
+  attr_accessor :name
+  @@all = []
   
   def initialize(name)
     @name = name
-    @songs = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
   def add_song(song)
-    @songs << song
     song.artist = self
-  end
-  
-  def songs
-    Song.all.select {|song| song.artist == self}
   end
 end
